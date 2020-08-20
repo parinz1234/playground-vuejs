7f71e7183d492608dcbc5f12259e5a4e6b0d3178
parinz1234 <parinya.ons@gmail.com>
2020-08-20 18:03:58 +0700

    feat: add example counter

---
+++ src/components/vuex/counter.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    Counter
+    <p>total {{ count }}</p>
+    <button @click="increment" type="button">+</button>
+    <br />
+    <button @click="decrement" type="button">-</button>
+  </div>
+</template>
+<script>
+import { mapState } from 'vuex'
+export default {
+  methods: {
+    increment() {
+      // commit `increment` action
+      this.$store.commit('increment')
+    },
+    decrement() {
+      // commit `decrement` action
+      this.$store.commit('decrement')
+    },
+  },
+  computed: {
+    ...mapState({
+      count: state => state.count,
+    }),
+  },
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,22 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
+Vue.use(Vuex)
 
-import Foo from './components/Foo.vue'
-import Bar from './components/Bar.vue'
+import Counter from './components/vuex/counter.vue'
+
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+  },
+  mutations: {
+    increment(state) {
+      state.count++
+    },
+    decrement(state) {
+      state.count--
+    },
+  },
+})
 
 const router = new VueRouter({
   mode: 'history',
-  routes: [
-    { path: '/foo', component: Foo },
-    { path: '/bar', component: Bar },
-  ],
+  routes: [{ path: '/vuex/counter', component: Counter }],
 })
 
 new Vue({
   render: h => h(App),
   router,
+  store,
 }).$mount('#app')
