4c95ae5f8aed52ad5188d97ad8e2a502f6d48e9c
parinz1234 <parinya.ons@gmail.com>
2020-08-20 15:36:36 +0700

    feat: initial vue router

---
+++ src/components/Bar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    Bar
+  </div>
+</template>
+<script>
+export default {
+  name: 'Bar',
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+import Foo from './components/Foo.vue'
+import Bar from './components/Bar.vue'
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/foo', component: Foo },
+    { path: '/bar', component: Bar },
+  ],
+})
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
